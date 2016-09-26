cd418f49d9eca454a8b3b1958448fb72def8ada7
Lei Wang <tendant@gmail.com>
2016-09-26 00:17:26 -0700

    add starwars demo schema

--- src/graphql_clj_starter/graphql.clj
+++ src/graphql_clj_starter/graphql.clj
@@ -38,7 +38,105 @@ type Query {
 }
 ")
 
-(def starter-resolver-fn nil)
+(def luke {:id "1000",
+           :name "Luke Skywalker"
+           :friends ["1002" "1003" "2000" "2001" ]
+           :appearsIn [ 4, 5, 6 ],
+           :homePlanet "Tatooine"})
+
+(def vader {:id "1001",
+            :name "Darth Vader"
+            :friends [ "1004" ]
+            :appearsIn [ 4, 5, 6 ]
+            :homePlanet "Tatooine"})
+
+(def han {
+          :id "1002",
+          :name "Han Solo",
+          :friends [ "1000", "1003", "2001" ],
+          :appearsIn [ 4, 5, 6 ],
+})
+
+(def leia {
+           :id "1003",
+           :name "Leia Organa",
+           :friends [ "1000", "1002", "2000", "2001" ],
+           :appearsIn [ 4, 5, 6 ],
+           :homePlanet "Alderaan",
+})
+
+(def tarkin {
+             :id "1004",
+             :name "Wilhuff Tarkin",
+             :friends [ "1001" ],
+             :appearsIn [ 4 ],
+             })
+
+(def humanData  {
+                 "1000" luke
+                 "1001" vader
+                 "1002" han
+                 "1003" leia
+                 "1004" tarkin
+                 })
+
+(def threepio {
+               :id "2000",
+               :name "C-3PO",
+               :friends [ "1000", "1002", "1003", "2001" ],
+               :appearsIn [ 4, 5, 6 ],
+               :primaryFunction "Protocol",
+               })
+
+(def artoo {
+            :id "2001",
+            :name "R2-D2",
+            :friends [ "1000", "1002", "1003" ],
+            :appearsIn [ 4, 5, 6 ],
+            :primaryFunction "Astromech",
+            })
+
+(def droidData {"2002" threepio
+                "2001" artoo})
+
+(defn get-character [id]
+  (or (get humanData id)
+      (get droidData id)))
+
+(defn get-friends [character]
+  (map get-character (:friends character)))
+
+(defn get-hero [episode]
+  (if (= episode 5)
+    luke
+    artoo))
+
+(defn get-human [id]
+  (get humanData id))
+
+(defn get-droid [id]
+  (get droidData id))
+
+(defn starter-resolver-fn [type-name field-name]
+  (match/match
+   [type-name field-name]
+   ["Query" "hero"] (fn [context parent & rest]
+                      (let [args (first rest)]
+                        (get-hero (:episode args))))
+   ["Query" "human"] (fn [context parent & rest]
+                       (let [args (first rest)]
+                         (get-human (str (get args "id")))))
+   ["Query" "droid"] (fn [context parent & rest]
+                       (let [args (first rest)]
+                         (get-droid (str (get args "id")))))
+   ;; Hacky!!! Should use resolver for interface
+   ["Human" "friends"] (fn [context parent & rest]
+                         (println parent)
+                         (get-friends parent))
+   ["Droid" "friends"] (fn [context parent & rest]
+                         (println parent)
+                         (get-friends parent))
+   :else nil))
 
 (def parsed-schema (parser/parse starter-schema))
 
