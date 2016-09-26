4a2c9bf56502e91beaabf83c5fdbf0b96efcd4f4
Lei Wang <tendant@gmail.com>
2016-09-25 21:50:47 -0700

    Add graphiql

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './graphiql.css';
+import GraphiQL from 'graphiql';
+import fetch from 'isomorphic-fetch';
 
 class App extends Component {
+  graphQLFetcher(graphQLParams) {
+    return fetch(window.location.origin + '/graphql', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <GraphiQL fetcher={this.graphQLFetcher} />
     );
   }
 }
