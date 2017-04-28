0d99308970a9ba6e486d06d194d868d98e83c37a
Lei Wang <tendant@gmail.com>
2017-04-27 20:46:04 -0700

    upgrade to graphql-clj 0.2.1

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [ring/ring-json "0.4.0"]
                  [ring-cors "0.1.8"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [graphql-clj "0.2.0" :exclusions [org.clojure/clojure]]
+                 [graphql-clj "0.2.1" :exclusions [org.clojure/clojure]]
                  [clojure-future-spec "1.9.0-alpha13"]]
   :main ^:skip-aot graphql-clj-starter.core
   :target-path "target/%s"
