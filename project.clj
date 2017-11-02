a1c665df87773c7279f0b9c4d286cc0d647de387
Victor Igor <victor.0w3@hotmail.com>
2017-11-01 23:03:36 -0200

    fixed version to graphql-clj

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [ring/ring-defaults "0.2.1"]
                  [ring/ring-json "0.4.0"]
                  [ring-cors "0.1.8"]
-                 [graphql-clj "0.2.4-SNAPSHOT"]]
+                 [graphql-clj "0.2.5"]]
   :main ^:skip-aot graphql-clj-starter.core
   :target-path "target/%s"
   :resource-paths ["build"]
